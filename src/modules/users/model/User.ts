3b91ed28a39ef5a4fabb1534a2fb51974066d326
Thiago Giammattey <giammattey@gmail.com>
2021-12-03 11:18:20 -0300

    config swagger

---
+++ src/modules/users/model/User.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidV4 } from "uuid";
+
+class User {
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    // eslint-disable-next-line prettier/prettier
+    if (!this.id)
+    {
+      this.id = uuidV4();
+      this.admin = false;
+    }
+  }
+}
+
+export { User };
